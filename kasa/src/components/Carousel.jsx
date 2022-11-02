b657f131869efe7a8e0a3311f0e97120772b19d5
laetitia <abs.amel@free.fr>
2022-11-02 19:28:06 +0100

    carrousel.jsx

--- kasa/src/components/Carousel.jsx
+++ kasa/src/components/Carousel.jsx
@@ -6,19 +6,21 @@ import arrowRight from "../images/arrowRight.png"
 //import CarouselBanner from '../images/CarouselBanner.png';
 
 const Carousel = (props) => {
-      const pictures = props.pictures;
-      const [currentIndex, setCurrentIndex] = useState(0);
+        const pictures = props.pictures;
+        const [currentIndex, setCurrentIndex] = useState(0);
 
-			const onTheRight = () => {
-			      const firstPicture = currentIndex === 0;
-			      const newIndex = firstPicture ? pictures.length -1 : currentIndex -1;
+			
+
+			const previousSlide = () => {
+			      const lastPicture = currentIndex === pictures.length -1;
+			      const newIndex = lastPicture ? 0  : currentIndex +1;
 			      setCurrentIndex(newIndex);
 			};
 
-			const onTheLeft = () => {
-			      const lastPicture = currentIndex === -1;
-			      const newIndex = lastPicture ? 0  : currentIndex +1;
-			      setCurrentIndex(newIndex);
+			const nextSlide = () => {
+				const firstPicture = currentIndex === 0;
+				const newIndex = firstPicture ? pictures.length -1 : currentIndex -1;
+			setCurrentIndex(newIndex);
 			};
 
       if (pictures.length === 1) {
@@ -31,15 +33,15 @@ const Carousel = (props) => {
       
             return (
 					<div className="carousel__slide">
-						<img className="carouselArrowLeft"src={arrowLeft} alt="Flèche gauche" onClick={onTheLeft}/>
+						<img className="carouselArrowLeft"src={arrowLeft} alt="Flèche gauche" onClick={nextSlide}/>
 						<ul>
 							{props.pictures.map((picture, index) =>
 								<li key={index}>
-									<img className="carousel__pictures" src={picture} alt="photos de l'appartement" />
+									<img className="carousel__pictures" src={pictures[currentIndex]} alt="photos de l'appartement" />
 								</li>
 							)}
 						</ul>
-						<img className="carouselArrowRight"src={arrowRight} alt="Flèche droite"onClick={onTheRight} />
+						<img className="carouselArrowRight"src={arrowRight} alt="Flèche droite"onClick={previousSlide} />
 					
                                     <span className="carousel__indicators">
                                           {currentIndex + 1}/{pictures.length}
@@ -55,3 +57,64 @@ const Carousel = (props) => {
 };
 
 export default Carousel;
+
+
+// const pictures = props.pictures;
+// const [SliderPosition, setSliderPosition] = useState(0);
+
+// 	const nextSlide = () => {
+// 		const newPosition = SliderPosition;
+// 		if (newPosition > 0) {
+// 			pictures.length = newPosition -1;
+// 		}
+// 		translateFullSlides(newPosition);
+// 		setSliderPosition(newPosition);
+// 	}
+// 	const previousSlide = () => {
+// 		const newPosition = SliderPosition;
+// 		if (newPosition < pictures.length -1) {
+// 			pictures.length = newPosition +1;
+// 		}
+// 		translateFullSlides(newPosition);
+// 		setSliderPosition(newPosition);
+// 	}
+// 		console.log(previousSlide)
+// 	const nextClickSlide = () => {
+// 		nextSlide();
+// 	   }
+
+// 	   const previousClickSlide = () => {
+// 		previousSlide();
+// 	   }
+
+// 	   const translateFullSlides = (newPosition) => {
+// 		for (let i = 0; i < pictures.length; i++) {
+// 			newPosition = pictures.length +1
+// 		}
+// 	}
+
+// 	const displayItems = 
+// 	<ul>
+// 		{props.pictures.map((picture, index) =>
+// 			<li key={index}>
+// 			<img className="carousel__pictures" 
+// 			src={picture} 
+// 			alt="photos de l'appartement" />
+// 			</li>
+// 		)}
+// 	</ul>
+
+// 	return (
+// 			<div className="carousel__slide">
+// 				<img className="carouselArrowRight"src={arrowRight} alt="Flèche droite"onClick={nextClickSlide} />
+				
+// 				<div>{displayItems}</div>
+				
+// 				<img className="carouselArrowLeft"src={arrowLeft} alt="Flèche gauche" onClick={previousClickSlide}/>
+				
+// 				<span className="carousel__indicators">
+// 					{SliderPosition + 1}/{pictures.length}
+// 				</span>		
+// 			</div>
+// 	)
+// }
